3583436beb2ac9552dcae1726ad2f311dfc83436
ingofehrenz <ingfeh1@msn.com>
2023-07-06 16:31:26 +0200

    erstellt

--- src/main/kotlin/Account.kt
+++ src/main/kotlin/Account.kt
@@ -1,2 +1,26 @@
-class Account {
+class CustomerAccount (username: String, password: String, shop: Shop) : Account(username, password, shop) {
+    private val cart: MutableList<Product> = mutableListOf()
+
+    fun addProductToCart() {
+        println("Artikel zum Warenkorb hinzufuegen:")
+        println("Bitte geben Sie den Namen des Produkts ein:")
+        val productName = readln()
+        val product = shop.products.find { it.name == productName }
+        if (product != null) {
+            card.add(product)
+            println("Produkt nicht gefunden.Bitte geben Sie einen gueltigen Namen ein.")
+        }
+    }
+    fun printCart() {
+        if (cart.isNotEmpty()) {
+            println("Warenkorbinhalt:")
+            for (product in cart) {
+                println("Name: ${product.name}")
+                println("Preis: ${product.price}")
+                println("----------------------------")
+            }
+        } else {
+            println("Der Warenkorb ist leer.")
+        }
+    }
 }
