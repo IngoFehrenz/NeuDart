0def12ed8a81eb919691119a277ae0a668d0a1be
ingofehrenz <ingfeh1@msn.com>
2023-08-13 22:33:16 +0200

    bearbeitet

---
+++ src/main/kotlin/Test.kt
@@ -0,0 +1,4 @@
+/*
+Bei diesem Code hat mir am Anfang Chat GPT,Google und Youtube Videos geholfen.
+Desweiteren haben mir Andreas Pelczer,Alexandros Theodoropoulos,Max Brockauf und Chris Seitz
+aus Batch 9 dabei geholfen.*/
