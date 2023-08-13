47dc252eeeabc5aaec52853fb68f4ba699294d1c
ingofehrenz <ingfeh1@msn.com>
2023-08-13 23:09:26 +0200

    bearbeitet

--- src/main/kotlin/Test.kt
+++ src/main/kotlin/Test.kt
@@ -1,6 +1,8 @@
 /*
-Bei diesem Code hat mir am Anfang Chat GPT,Google und Youtube Videos geholfen.
-Desweiteren haben mir Andreas Pelczer,Alexandros Theodoropoulos,Max Brockauf und Chris Seitz
-aus Batch 9 dabei geholfen.
-
+Bei diesem Code hat mir am Anfang Chat GPT,Google und Youtube Videos geholfen. Das war auch der
+Shop denn ich in Batch 9 als Abschlußprojekt benutzt hatte, da hatte ich aber nur die Main.kt,
+die Account.kt, die Product.kt, und die Shop.kt.
+Jetzt habe ich die fehlenden 6 Classen und Funktionen noch hinzugefügt.
+Des weiteren haben mir Andreas Pelczer, Alexandros Theodoropoulos, Max Brockauf und Chris Seitz
+aus Batch 9 dabei geholfen. Da wir immer noch zusammen arbeiten.
 */
